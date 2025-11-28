664bfe11caf197bcd9f8edb8266d1f47ea0182b6
Pedro Ap <pedroaparecidori@gmail.com>
2025-11-28 13:26:52 -0300

    Fix/ apenas atualizando que isso é apenas um site educativo :)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -19,7 +19,7 @@ export const Route = createFileRoute('/')({
 function App() {
 
   const getTodos = async () => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${id}`)
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos`)
     return await response.json()
   }
   
@@ -31,9 +31,17 @@ function App() {
 //o uso do isPending ou isLoading é "tudo a mesma coisa"
 //mas o uso do refetch, pede auxilio da memoria cache, muito perpiscaz
   return (
-    <div className='flex flex-col'>
-      {query.isPending ? "loading" : JSON.stringify(query.data.slice(0, 10))}
-      <button onClick={() => query.refetch()} className='p-[10px] bg-blue-300 hover:cursor-pointer'>Refetch</button>
+    <div className='flex flex-col justify-center items-center'>
+      <div className='flex flex-col'>
+        {query.isPending ? "loading" : JSON.stringify(query.data.slice(0, 10))}
+        <button onClick={() => query.refetch()} className='p-[10px] bg-blue-300 hover:cursor-pointer'>Refetch</button>
+      </div>
+      <div className='flex justify-center items-center p-[20px] m-0 w-[400px] h-[200px] bg-green-300 mt-[40px]'>
+        Não tenho um portfólio
+      </div>
+      <div className='flex justify-center items-center p-[20px] m-0 w-[400px] h-[200px] bg-yellow-200 mt-[40px]'>
+        Isso é só um exemplo que eu consigo usar as libs e ferramentas requiridas!
+      </div>
     </div>
   )
 }
